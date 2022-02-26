da6b00a8ec1ea3d24f2d0b865106ef7ed31842b0
Eduard-747 <88149958+Eduard-747@users.noreply.github.com>
2022-02-26 21:20:51 +0400

    Add files via upload

---
+++ SET/set.h
@@ -0,0 +1,35 @@
+#ifndef SET_H
+#define SET_H
+
+#include "tree.h"
+
+#include <initializer_list>
+
+namespace myl {
+    template <typename T>
+    class set
+    {
+    public:
+        set() = default;
+        set(const set&);
+        set(set&&);
+        set& operator=(const set&);
+        set& operator=(set&&);
+        set(const std::initializer_list<T>&);
+        set& operator=(const std::initializer_list<T>&);
+    public:
+        const T& find(const T&) const;
+        void insert(const T&);
+        void erase(const T&);
+        void clear();
+        size_t size() const;
+        void print() const;
+    private:
+        myl::AVLtree<T> head;
+        size_t _size;
+    };
+}
+
+#include "set.hpp"
+
+#endif
