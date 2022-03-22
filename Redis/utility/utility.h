8670f0511e750f3c80cdf21a82f4a962125ffe32
Eduard-747 <88149958+Eduard-747@users.noreply.github.com>
2022-03-22 17:53:44 +0400

    Update utility.h

--- Redis/utility/utility.h
+++ Redis/utility/utility.h
@@ -7,7 +7,7 @@
 
 struct Utility
 {
-   /* static int stoi(const String& str) 
+    static int stoi(const String& str) 
     {
  	   vector<int> vec;
  	   for (int i = 0; i < str.size(); ++i) 
@@ -20,7 +20,7 @@ struct Utility
  		   res += vec[i] * pow(10, vec.getSize() - i - 1);
 	   }
 	   return res;
-   }*/
+   }
     static unsigned int my_strlen(const char *s);
 };
 
